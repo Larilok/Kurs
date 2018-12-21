66f165598e1436fbde2d1bf8f3d26c85f948cc04
IlyaLaska <IlyaLaska@users.noreply.github.com>
2018-12-21 22:52:00 +0200

    Added net

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 'use strict';
 
+let net = require('net');
+
+// let socket = new net.Socket();
+// socket.connect({port:443});
+let soc = net.createConnection({port: 80});
+
 //scan ports hosts
 
-function log(...args) {return console.log(...args)}
+function log(...args) {
+    return console.log(...args)
+}
 
 console.log(process.argv);
 
@@ -11,7 +19,7 @@ function parsePorts(hosts) {
     // console.log(hosts);
     let hostIntervals = [];
     hosts.forEach(host => {
-        if(host.indexOf('-') !== -1) {//working on intervals
+        if (host.indexOf('-') !== -1) {//working on intervals
             // console.log(host);
             host.split('-').forEach(h => hostIntervals.push(h));
             hosts.splice(hosts.indexOf(host), 1);
@@ -19,12 +27,12 @@ function parsePorts(hosts) {
     });
     // console.log(hosts);
     // console.log(hostIntervals);
-    if(hostIntervals.length % 2 !== 0) throw new Error("Trailing interval in hosts");
+    if (hostIntervals.length % 2 !== 0) throw new Error("Trailing interval in hosts");
     return [hosts, hostIntervals];
 }
 
 function parseArgs() {
-    if(process.argv.length === 2 || process.argv[2] === "help") {
+    if (process.argv.length === 2 || process.argv[2] === "help") {
         log(`Port scanner help:
         Use this tool to check for open ports on one or more TCP/UDP host
         Use:
@@ -37,11 +45,12 @@ function parseArgs() {
     }
 
 
-    if(process.argv[2]) log(parsePorts(process.argv[2]));
+    if (process.argv[2]) log(parsePorts(process.argv[2]));
     // parseHosts(process.argv[3]);
 
 }
 
-/*Main()*/{
+/*Main()*/
+{
     parseArgs();
 }
